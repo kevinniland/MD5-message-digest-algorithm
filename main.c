5e000448cf2a20f4c97575f6eb372deeaa536bb3
Kevin Niland <G00342279@gmit.ie>
2020-01-13 17:16:15 +0000

    Reading from file implemented

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-const char *string = "Testing the MD5 hash algorithm";
+#pragma warning(disable:4996)
 
 int main() {
-	int i = 5;
+	char fileName[100] = "fileToHash.txt";
+	char line;
+	int i, bytes;
 
-	printf(string);
+	// Need to change to fopen_s
+	FILE *file = fopen(fileName, "r");
+
+	if (file == NULL) {
+		printf("ERROR: %s cannot be opened", fileName);
+		exit(0);
+	} 
+	line = fgetc(file);
+
+	while (line != EOF) {
+		printf("%c", line);
+
+		line = fgetc(file);
+	}
+
+	fclose(file);
+
 	getch();
+
+	return 0;
 }
