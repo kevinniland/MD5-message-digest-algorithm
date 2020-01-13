7e6a583f6ee92a4eb69d0db318d711c0f468604d
Kevin Niland <G00342279@gmit.ie>
2020-01-13 16:12:52 +0000

    MD5 algorithm started

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *string = "Testing the MD5 hash algorithm";
+
+int main() {
+	int i = 5;
+
+	printf(string);
+	getch();
+}
